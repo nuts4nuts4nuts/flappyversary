42cacea83aab0908dc34986cff17c8b0fe8650c2
David Johnston <dvdjoh@gmail.com>
2023-05-29 17:36:10 -0400

    Fix target-ball signal

--- scripts/target-ball.gd
+++ scripts/target-ball.gd
@@ -3,37 +3,34 @@ extends RigidBody2D
 signal merged
 
 @export var initial_impulse: Vector2
-@export var _gravity_well: Node2D
+@export var gravity_well: Node2D
 
 var merge_boost = 0.2
-var ball_value = 5
-var force = 90
+var ball_value = 2
+var base_force = 50
 var is_target = true
 var target_progress = 0
 var cashing_in = false
 
 func _ready():
-	
 	apply_central_impulse(initial_impulse)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _physics_process(delta):
-	if(_gravity_well != null):
-		var _dist = (_gravity_well.position.distance_to(position))
-		print(_dist)
-		apply_force((1/pow(_dist, 2.2)) * (_gravity_well.position - position).normalized() * force, Vector2.ZERO)
-		
-		
-		#linear_velocity *= 0.99
+	if(gravity_well != null):
+		var me_to_well = gravity_well.global_position - global_position
+		var distance = me_to_well.length_squared()
+		var direction = me_to_well.normalized()
+
+		var force = base_force * direction
+		var falloff = 1 / max(1, (distance / 10000))
+		apply_force(force * falloff, Vector2.ZERO)
+
 
 func _on_body_entered(body):
 	var my_velocity = linear_velocity
 	var their_velocity = body.linear_velocity
-	if !body.is_target && body.ball_value == ball_value:
+	if body.ball_value == ball_value:
 		print(name + " stealing")
 		apply_central_impulse((my_velocity + their_velocity) * 0.2)
 		ball_value += ball_value
@@ -50,7 +47,7 @@ func _on_body_entered(body):
 			start_cash_in()
 	else:
 		apply_central_impulse((my_velocity + their_velocity) * 0.2)
-	
+
 
 func start_cash_in():
 	get_node("Timer").start()
