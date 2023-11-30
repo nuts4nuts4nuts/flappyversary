ee3b3d389ec78e82cacff9833e2c4e29f029aaff
williecheng <williett123@gmail.com>
2023-11-29 20:08:25 -0800

    Added start button

---
+++ scripts/HUD.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+var _parent
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_parent = get_parent()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	_parent.start_game()
