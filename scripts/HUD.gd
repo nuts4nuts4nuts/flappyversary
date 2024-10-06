f812a3e970640ef28dd61116dcda066d9330c40b
David Johnston <dvdjoh@gmail.com>
2024-10-06 18:20:17 -0400

    Add share button on end screen

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -5,18 +5,29 @@ var _parent
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_parent = get_parent()
-	pass # Replace with function body.
-
+	$ShareButton	.hide()
 
 func _on_start_button_pressed():
+	$StartButton.hide()
+	$ShareButton	.hide()
 	if $StartButton.text == "Start":
-		$StartButton.hide()
 		_parent.start_game()
 	else:
-		$StartButton.hide()
 		_parent.restart_game()
+
+
+func _on_share_button_pressed() -> void:
+	screenshot()
 
 
 func show_restart():
 	$StartButton.text = "Restart"
 	$StartButton.show()
+	$ShareButton	.show()
+
+
+func screenshot():
+	var capture = get_viewport().get_texture().get_image()
+	var _time = Time.get_datetime_string_from_system()
+	var filename = OS.get_data_dir() + "/Screenshot-{0}.png".format({"0":_time})
+	capture.save_png(filename)
