96e1fecaf8d4015c56f93d4e7d06c558a9c0af6c
williecheng <williett123@gmail.com>
2025-04-06 15:28:16 -0700

    Added option to spawn balls on cash-in

--- scenes/ball_spawn_warning.gd
+++ scenes/ball_spawn_warning.gd
@@ -1,11 +1,31 @@
 extends Sprite2D
+var main
 
-var flicker_timer
+var value
+signal expired(pos, value)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	flicker_timer 
+	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
+
+func init(_time, _value):
+	value = _value
+	$visibility_timer.wait_time = _time
+	$visibility_timer.start()
+
+func _on_despawn_timer_timeout() -> void:
+	expired.emit(global_position, value)
+	queue_free()
+
+
+func _on_visibility_timer_timeout() -> void:
+	print("VISIBLE NOW")
+	visible = true
+	$despawn_timer.start()
