beaf3bbc1f9220080bde291c03f62ea0397800bf
David Johnston <dvdjoh@gmail.com>
2026-05-17 16:40:40 -0400

    Merged spawning style

--- scenes/ball_spawn_warning.gd
+++ scenes/ball_spawn_warning.gd
@@ -2,28 +2,26 @@ extends Sprite2D
 var main
 
 var value
-signal expired(pos, value)
+var impulse
+signal expired(pos, value, impulse)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	pass
 
-func init(_time, _value):
+func init(_time, _value, _impulse):
 	value = _value
+	impulse = _impulse
 	$visibility_timer.wait_time = _time
 	$visibility_timer.start()
 
 func _on_despawn_timer_timeout() -> void:
-	expired.emit(global_position, value)
+	expired.emit(global_position, value, impulse)
 	queue_free()
-
 
 func _on_visibility_timer_timeout() -> void:
 	print("VISIBLE NOW")
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-enum SPAWNING_ALGORITHM {PureRandom, DontRepeat, CountUp}
+enum SPAWNING_ALGORITHM {PureRandom, DontRepeat, CountUp, One}
 var spawn_funcs = {
 	SPAWNING_ALGORITHM.PureRandom: algo_pure_random,
 	SPAWNING_ALGORITHM.DontRepeat: algo_dont_repeat,
 	SPAWNING_ALGORITHM.CountUp: algo_count_up,
+	SPAWNING_ALGORITHM.One: algo_one,
 }
 enum WELL_PROFILE {Standard, FastAndClose, Strong}
 @export var well_profile : WELL_PROFILE
@@ -67,7 +68,6 @@ func _ready():
 	$HUD.start_requested.connect(start_game)
 	$HUD.restart_requested.connect(restart_game)
 
-
 func _process(delta):
 	$SpawnTimer.wait_time = spawn_interval
 	spawn_pos_ratio += delta * spawn_pos_orbit_speed
@@ -79,7 +79,6 @@ func _process(delta):
 		update_death_danger_state()
 
 	queue_redraw()
-
 
 func update_death_danger_state():
 	var max_death_time: float = 0.0
@@ -99,10 +98,8 @@ func update_death_danger_state():
 		GameEvents.target_ball_safe.emit()
 		was_any_ball_dying = false
 
-
 func _draw():
 	draw_incoming_ball_indicator(ball_spawn_pos(get_viewport_rect(), spawn_pos_ratio), $SpawnTimer.time_left)
-
 
 func _unhandled_key_input(event: InputEvent):
 	# DEBUG STUFF
@@ -121,7 +118,6 @@ func _unhandled_key_input(event: InputEvent):
 			Engine.time_scale -= 0.25
 			print("Engine timescale: " + str(Engine.time_scale))
 
-
 func start_game():
 	GameEvents.game_restarting.emit()  # Balls self-cleanup
 	game_running = true
@@ -134,19 +130,15 @@ func start_game():
 	spawn_starting_ball()
 	$SpawnTimer.start()
 
-
 func spawn_starting_ball():
 	var start_pos = get_viewport_rect().size * Vector2(0.5, 0.33)
 	spawn_ball(start_pos, 1, Vector2.ZERO)
 
-
 func restart_game():
 	start_game()
 
-
 func sleep(sec):
 	await get_tree().create_timer(sec).timeout
-
 
 func end_game():
 	$gravity_well.deactivate()
@@ -158,7 +150,6 @@ func end_game():
 		was_any_ball_dying = false
 		GameEvents.target_ball_safe.emit()
 	celebrate_winner_balls()
-
 
 func celebrate_winner_balls():
 	var center = get_viewport_rect().get_center()
@@ -177,16 +168,13 @@ func celebrate_winner_balls():
 		create_tween().tween_property(winner, "position", center, 0.5) \
 			.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
 
-
 func _on_ball_leaving_screen(_ball):
 	end_game()
-
 
 func _on_cluster_merged(new_value, _pos):
 	if new_value > high_score:
 		high_score = new_value
 		GameEvents.high_score_updated.emit(high_score)
-
 
 func _on_cluster_cashing_in(cluster_id: int, _value: int, _ball_count: int, duration: float):
 	if cash_in_timers.has(cluster_id):
@@ -196,10 +184,8 @@ func _on_cluster_cashing_in(cluster_id: int, _value: int, _ball_count: int, dura
 	timer.init(cluster_id, duration)
 	cash_in_timers[cluster_id] = timer
 
-
 func _on_cluster_cashed_in_spawn_balls(_new_value, _pos):
 	populate_with_new_balls()
-
 
 func _on_spawn_timer_timeout():
 	var direction = (get_viewport_rect().get_center() - ball_spawn_pos(get_viewport_rect(), spawn_pos_ratio)).normalized()
@@ -208,16 +194,19 @@ func _on_spawn_timer_timeout():
 	spawn_ball(ball_spawn_pos(get_viewport_rect(), spawn_pos_ratio), last_generated_number, direction * velo)
 
 func populate_with_new_balls():
+	var ball_count = ball_spawner_rng.randi_range(1, 2)
+	var rect = get_viewport_rect().size
 	var delay = cashin_ball_spawn_initial_delay
-	for x in range(3):
-		for y in range(3):
-			var warning = ball_spawn_warning_scene.instantiate()
-			add_child(warning)
-			warning.global_position = Vector2(x * 1200 + 700 + ball_spawner_rng.randi_range(cash_in_ball_spawn_offset_min, cash_in_ball_spawn_offset_max), y * 700 + 500 + ball_spawner_rng.randi_range(cash_in_ball_spawn_offset_min, cash_in_ball_spawn_offset_max))
-			warning.expired.connect(spawn_ball)
-			warning.init(delay, generate_new_number(last_generated_number))
-			delay += cashin_ball_spawn_initial_delay
-
+	for x in range(ball_count):
+		var pos = Vector2(ball_spawner_rng.randi_range(0, rect.x), ball_spawner_rng.randi_range(0, rect.y))
+		var direction = (get_viewport_rect().get_center() - pos).normalized()
+		var velo = ball_spawner_rng.randf_range(100, 200)
+		var warning = ball_spawn_warning_scene.instantiate()
+		add_child(warning)
+		warning.global_position = pos
+		warning.expired.connect(spawn_ball)
+		warning.init(delay, generate_new_number(last_generated_number), direction * velo)
+		delay += cashin_ball_spawn_initial_delay
 
 func generate_new_number(previous_number):
 	var top_value = max(1, int(ceil(max_spawn_value_methods[max_spawn_value_method].call(high_score))))
@@ -226,12 +215,10 @@ func generate_new_number(previous_number):
 	last_generated_number = spawn_funcs[spawn_algorithm].call(max_power, previous_number)
 	return last_generated_number
 
-
 func algo_pure_random(max_power, previous_number):
 	# Return a random power of 2 from 1 to 2^max_power
 	var power = ball_spawner_rng.randi_range(0, max_power)
 	return int(pow(2, power))
-
 
 func algo_dont_repeat(max_power, previous_number):
 	# Return a random power of 2 different from previous
@@ -243,13 +230,14 @@ func algo_dont_repeat(max_power, previous_number):
 		new_power += 1
 	return int(pow(2, new_power))
 
-
 func algo_count_up(max_power, previous_number):
 	# Count up through powers of 2
 	var prev_power = int(round(log(max(1, previous_number)) / log(2)))
 	var new_power = (prev_power + 1) % (max_power + 1)
 	return int(pow(2, new_power))
 
+func algo_one(max_power, previous_number):
+	return 1
 
 func spawn_ball(pos, value = 1, impulse = Vector2.ZERO):
 	var ball = ball_scene.instantiate()
@@ -259,13 +247,11 @@ func spawn_ball(pos, value = 1, impulse = Vector2.ZERO):
 	ball.ball_value = value
 	add_child(ball)
 
-
 func draw_incoming_ball_indicator(pos, time_to_spawn):
 	var ball_size = 32.0
 	var portion_of_spawn_timer_remaining = time_to_spawn / $SpawnTimer.wait_time
 	var ball_scale = 1.0 - portion_of_spawn_timer_remaining
 	draw_circle(pos, ball_size * ball_scale, Color.RED)
-
 
 func ball_spawn_pos(rect: Rect2, t: float):
 	var aspect_ratio = rect.size.x / rect.size.y
